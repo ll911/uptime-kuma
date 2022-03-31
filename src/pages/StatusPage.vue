97df5982d30de5950ffc26585bf15857323d1716
leo <leo.lou@gov.bc.ca>
2022-03-31 10:24:20 -0700

    Update StatusPage.vue

--- src/pages/StatusPage.vue
+++ src/pages/StatusPage.vue
@@ -227,7 +227,6 @@
             </div>
 
             <footer class="mt-5 mb-4">
-                {{ $t("Powered by") }} <a target="_blank" href="https://github.com/louislam/uptime-kuma">{{ $t("Uptime Kuma" ) }}</a>
             </footer>
         </div>
 
